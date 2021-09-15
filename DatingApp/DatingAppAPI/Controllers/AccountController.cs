2aa10dc5426fede9d1890a807d0cc882b5afb556
Sonu <sonusaha110@gmail.com>
2021-09-15 22:55:37 +0530

    Add login endpoint and login DTO

--- DatingApp/DatingAppAPI/Controllers/AccountController.cs
+++ DatingApp/DatingAppAPI/Controllers/AccountController.cs
@@ -42,6 +42,24 @@ namespace DatingAppAPI.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.UserName == loginDTO.Username);
+
+            if (user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for (int i=0; i<computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(u => u.UserName == username);
